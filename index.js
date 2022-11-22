82675bd7bfaa224ba21ebee3ed487f3b10b13623
stanislavBenet <stanislav.benet@gmail.com>
2022-11-22 19:42:47 +0200

    Add edit homework 16.11 forIn

--- index.js
+++ index.js
@@ -4,19 +4,36 @@
 // Інформацію про особу запитувати у користувача.
 // Вивести отриманий об'єкт за допомогою for..in
 
-const getFamilyPerson = function () {
-  const selectFamily = prompt("Enter name of person:");
-  const family = {
-    Maksim: "brother",
-    Olha: "mother",
-    Ludmila: "sister",
-    Vasya: "grandfather",
-    Oleh: "father",
-  };
-  for (const key in family) {
-    if (key === selectFamily) {
-      return `${key} it\`s ${family[key]}`;
-    }
-  }
-};
-console.log(getFamilyPerson());
+/*                           прошлое ДЗ (неправильное)
+// const getFamilyPerson = function () {
+//   const selectFamily = prompt("Enter name of person:");
+//   const family = {
+//     Maksim: "brother",
+//     Olha: "mother",
+//     Ludmila: "sister",
+//     Vasya: "grandfather",
+//     Oleh: "father",
+//   };
+//   for (const key in family) {
+//     if (key === selectFamily) {
+//       return `${key} it\`s ${family[key]}`;
+//     }
+//   }
+// };
+// console.log(getFamilyPerson()); */
+
+
+
+ //                          EDIT HOMEWORK
+let family = {};
+let count = Number(prompt("How much family person you want writing?"));
+while (count > 0) {
+  let key = prompt("Enter name of person:");
+  let value = prompt("Enter role of person:");
+  family[key] = value;
+  count--;
+}
+
+for (const key in family) {
+  console.log(`${key} it\`s ${family[key]}`);
+}
